a87b62bbac4d504b18edf0eac8513360365938b0
AlwinJacobThomas <kailath.ajt@gmail.com>
2023-12-30 15:06:38 +0530

    changed url of video

--- src/pages/Section1.jsx
+++ src/pages/Section1.jsx
@@ -8,8 +8,8 @@ export default function Section1() {
         <h3 className="section1-subtitle">Pray with Mama Mary</h3>
         <p className="section1-desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, molestiae! Ratione itaque culpa, eaque laboriosam praesentium eos, expedita animi, tempora quaerat debitis sed odit! Blanditiis vero dolores debitis provident soluta.</p>
       </div>
-      <iframe className="video">
-         {/* src="https://www.youtube.com/embed/tgbNymZ7vqY"> */}
+      <iframe className="video"
+         src="https://youtu.be/m77D52rIwVE?si=7wbqRbslHPatUm49">
       </iframe>
       {/* <img className='bg' src="https://img.freepik.com/premium-photo/map-vancouver-with-red-pin-pointing-left_866663-10517.jpg" alt="bg" /> */}
     </div>
